544a0036e572a73c25729b76159f0b7c18155f6d
Huynha1 <81626666+Huynha1@users.noreply.github.com>
2022-05-10 22:14:07 +1000

    updates

--- Player.cpp
+++ Player.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include "Player.h"
 #include "TileBag.h"
 
@@ -9,9 +8,9 @@ Player::Player(std::string name, int score, TileBag* tb) {
     this->score = score;
     this->hand = new LinkedList();
     while (this->hand->size() < SCRABBLE_HAND) {
-        std::cout << "loop" << std::endl;
+        //std::cout << "loop" << std::endl;
         Node* tile = tb->getTile();
-        std::cout << "in loop" << std::endl;
+        //std::cout << "in loop" << std::endl;
         this->hand->addBack(tile);
     }
 }
@@ -128,129 +127,4 @@ void Player::removeTile(Node* node) {
 
     //Delete from hand
     this->hand->deleteAt(replacementIndex);
-=======
-#include "Player.h"
-#include "TileBag.h"
-
-//Constructor.
-//Player's hand is initially empty. Should call fillHand() once game starts.
-Player::Player(std::string name) {
-    this->name = name;
-    this->score = 0;
-    this->hand = new LinkedList();
-}
-
-//Deconstructor
-Player::~Player() {
-    delete hand;
-}
-
-//Returns player's name.
-std::string Player::getName() {
-    return this->name;
-}
-
-//Returns player's score.
-int Player::getScore() {
-    return this->score;
-}
-
-//Returns player's hand.
-LinkedList* Player::getHand() {
-    return this->hand;
-}
-
-//Print Function.
-//Should only be used for debugging purposes.
-void Player::printPlayer() {
-    std::cout << "Player: " << this->name << " Score: " << this->score << " Hand: " << std::endl;
-    this->hand->printList();
-}
-
-void Player::increaseScore(int value) {
-    this->score += value;
-}
-
-void Player::printHand() {
-    for (int i = 0; i < this->hand->size(); i++) {
-        std::cout << this->hand->get(i)->tile.letter << "-" << this->hand->get(i)->tile.value;
-        if (i == this->hand->size() - 1) {
-            std::cout << std::endl;
-        }
-        else {
-            std::cout << ", ";
-        }
-    }
-}
-
-//Initialize player's hand
-void Player::fillHand(TileBag* tb) {
-    while (this->hand->size() < SCRABBLE_HAND) {
-        Node* tile = tb->getTile();
-        this->hand->addBack(tile);
-    }
-}
-
-//Replace a tile by removing from hand and grabbing a new one from the Scrabble LinkedList.
-void Player::replaceTile(TileBag* tb, char letter) {
-    letter = toupper(letter);
-    //Find position of tile to replace from hand
-    int replacementIndex = -1;
-    Tile tile;
-    tile.letter = letter;
-    tile.value = 0;
-    
-    Node* tempTile = new Node(tile);
-    replacementIndex = this->hand->search(tempTile);
-
-    if (replacementIndex == -1) {
-        throw std::invalid_argument("Tile does not exist in player hand.");
-    }
-
-    delete tempTile;
-
-    //Delete from hand
-    this->hand->deleteAt(replacementIndex);
-
-    //Get new tile and put in original index
-    Node* newTile = tb->getTile();
-    this->hand->addAt(newTile, replacementIndex);
-}
-
-Node* Player::getTile(char letter) {
-    letter = toupper(letter);
-    //Find position of tile to replace from hand
-    int replacementIndex = -1;
-    Tile tile;
-    tile.letter = letter;
-    tile.value = 0;
-    
-    Node* tempTile = new Node(tile);
-    Node* returnTile = nullptr;
-    replacementIndex = this->hand->search(tempTile);
-
-    if (replacementIndex == -1) {
-        throw std::invalid_argument("Tile entered does not exist in the player's hand.");
-    }
-    else {
-        returnTile = this->hand->get(replacementIndex);
-    }
-
-    delete tempTile;
-    return returnTile;
-}
-
-void Player::removeTile(Node* node) {
-    //Find position of tile to replace from hand
-    int replacementIndex = -1;
-
-    replacementIndex = this->hand->search(node);
-
-    if (replacementIndex == -1) {
-        throw std::invalid_argument("Tile does not exist in player hand.");
-    }
-
-    //Delete from hand
-    this->hand->deleteAt(replacementIndex);
->>>>>>> 4027bd96efadd948298dcb95d12c57b8eaf7133a
 }
