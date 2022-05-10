ffe8b126029b92cf1358d9c0a6dbe3b7b7dffff1
Huynha1 <81626666+Huynha1@users.noreply.github.com>
2022-05-10 22:20:13 +1000

    udpates

--- Player.h
+++ Player.h
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #ifndef ASSIGN2_PLAYER
 #define ASSIGN2_PLAYER
 
@@ -44,51 +43,4 @@ private:
     LinkedList* hand;
 };
 
-=======
-#ifndef ASSIGN2_PLAYER
-#define ASSIGN2_PLAYER
-
-#include <string>
-#include <iostream>
-#include "LinkedList.h"
-#include "TileBag.h"
-
-#define SCRABBLE_HAND 7
-
-class Player {
-public:
-
-    //Constructor and Deconstructor
-
-    Player(std::string name);
-    ~Player();
-
-    //Get methods
-
-    std::string getName();
-    int getScore();
-    LinkedList* getHand();
-
-    void increaseScore(int value);
-
-    //Game functions
-
-    void fillHand(TileBag* tb);
-    void replaceTile(TileBag* tb, char letter);
-    void removeTile(Node* node);
-    void printHand();
-    Node* getTile(char letter);
-
-    
-    //Misc functions
-    
-    void printPlayer();
-
-private:
-    std::string name;
-    int         score;
-    LinkedList* hand;
-};
-
->>>>>>> 4027bd96efadd948298dcb95d12c57b8eaf7133a
 #endif
--- TileBag.h
+++ TileBag.h
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #ifndef ASSIGN2_TILEBAG
 #define ASSIGN2_TILEBAG
 
@@ -28,34 +27,4 @@ private:
     LinkedList* tileBag;
 };
 
-=======
-#ifndef ASSIGN2_TILEBAG
-#define ASSIGN2_TILEBAG
-
-#include "LinkedList.h"
-#include <iostream>
-#include <string>
-#include <fstream>
-
-class TileBag {
-public:
-
-    //Constructor and Deconstructor
-
-    TileBag();
-    ~TileBag();
-
-    //Misc Functions
-
-    Node* getTile();
-    void printBag();
-    LinkedList* getTileBag();
-
-private:
-    // Should not be called by other classes.
-    void shuffle(LinkedList* nodeList);
-    LinkedList* tileBag;
-};
-
->>>>>>> 4027bd96efadd948298dcb95d12c57b8eaf7133a
 #endif
